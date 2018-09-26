881a0fae26e8143a63ebbd789645f80fd02f84db
Mayank Khanna <khanna98@outlook.com>
2018-09-26 10:48:23 +0530

    First Program

---
+++ helloWorld.kt
@@ -0,0 +1,9 @@
+/*
+* We declare a package-level function main which returns Unit and takes
+* an Array of Strings as a parameter.
+* Semi-colons are optional !
+*/
+
+fun main(args: Array<String>){
+	println("Hello, World")
+}
